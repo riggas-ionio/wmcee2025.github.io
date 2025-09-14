4ebd5f4bca48978d8a29708c03d6bde39eb7ddd9
Dimitrios Ringas <55480871+riggas-ionio@users.noreply.github.com>
2025-09-14 22:51:24 +0300

    Send announcements per channel

--- _telegram/announcements.py
+++ _telegram/announcements.py
@@ -5,13 +5,17 @@ from decouple import config
 from datetime import datetime as DT
 import pytz
 import time
+import json
+
 
 global bot
 global TOKEN
 
 TOKEN = config('BOT_TOKEN')
-CHAT_ID = config('CHAT_ID')
-# ANNOUNCEMENTS = config('ANNOUNCEMENTS')
+# CHAT_ID = config('CHAT_ID')
+CHAT_ID_json = os.getenv("CHAT_ID")
+print(CHAT_ID_json)
+CHAT_ID = json.loads(CHAT_ID_json)
 ANNOUNCEMENTS = '_telegram/announcements.yaml'
 ANNOUNCE_EVERY = 15 #minutes
 
@@ -27,9 +31,9 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 NOW = DT.now(pytz.timezone('Europe/Athens')).replace(tzinfo=None)
 print('Now in Athens/GR: ', NOW)
 
-for dt, arr in parsed_announcements.items():
+for dt, channels in parsed_announcements.items():
     ann_time = DT.strptime(dt, '%d/%m/%Y %H:%M:%S')
-    print(ann_time)
+    # print('Announcement time:', ann_time)
     diff = (ann_time - NOW)
     # print ('Diff DAYS:', diff_sec.days )
     # print ('Diff SEC :', diff_sec.seconds )
@@ -40,7 +44,13 @@ for dt, arr in parsed_announcements.items():
         print('Skip: ', ann_time, '> ',ANNOUNCE_EVERY, ' MINUTES ', diff)
         continue
     print('Announce: ', ann_time)
-    for txt in arr:
-        # bot.send_message(chat_id=CHAT_ID, text=('Coming up at #wmcee #wmcee2025 \n🕰️'+(ann_time.strftime('%H:%M'))+' \n\n'+txt))
-        bot.send_message(chat_id=CHAT_ID, text=('Coming up at Wikimedia CEE Meeting 2025 \n🕰️'+(ann_time.strftime('%H:%M'))+' \n\n'+txt))
-        time.sleep(1)
+    for channel in channels:
+        if channel in CHAT_ID:
+            for txt in parsed_announcements[dt][channel]:
+                print('Coming up at #wmceem2025 \n\n🕰️ '+(ann_time.strftime('%H:%M %Y-%m-%d'))+' \n'+txt)
+                bot.send_message(chat_id=CHAT_ID[channel], text=('Coming up at Wikimedia CEE Meeting 2025 \n\n🕰️'+(ann_time.strftime('%H:%M %Y-%m-%d'))+' \n'+txt))
+                time.sleep(1)
+        else:
+            print('No channel:', channel)
+            continue
+            
